1adffe57203fbf1c4ffa11807a71c5a035cc2789
manasmagdum <37959958+manasmagdum@users.noreply.github.com>
2018-04-01 18:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask,request
-import json
+print("helloo")
+#from flask import Flask,request
+#import json
 #import requests
 #import ast
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
 @app.route('/',methods=['POST'])
-def JsonHandler():
-    flag=0
-    RequestDict={}
-    ProfileDict={}
-    print('Is this JSON?',request.is_json)
-    #print(request.headers)
-    content=request.get_json()
-    print(content)
-    res = {"text":"Got it! From my estimation, your days to solve."}  
-    print('Done')
-    return json.dumps(res)
+#def JsonHandler():
+#    flag=0
+#    RequestDict={}
+#    ProfileDict={}
+#    print('Is this JSON?',request.is_json)
+#    #print(request.headers)
+#    content=request.get_json()
+#    print(content)
+#    res = {"text":"Got it! From my estimation, your days to solve."}  
+#    print('Done')
+#    return json.dumps(res)
 
     
 
 
     
     
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
