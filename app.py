d24b4968e1ae2b7a0177a8793fdf7626596f48bf
manasmagdum <37959958+manasmagdum@users.noreply.github.com>
2018-04-01 18:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-html = 'Hello World!'
-return html
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def source():
+ html = 'Hello World!'
+ return html
 #from flask import Flask,request
 #import json
 #import requests
